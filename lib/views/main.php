9b82f14f2e300c74e5cc10a902bfdd87363f9415
CrazyCoder <true.isnt.false@gmail.com>
2013-12-18 16:12:27 +0700

    very simple view

--- lib/views/main.php
+++ lib/views/main.php
@@ -10,12 +10,44 @@
                 padding: 80px;
                 background-color: #eee;
             }
+            
+            .mput-tests {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+            }
+            
+            .mput-test {
+                font-size: 24px;
+            }
+            
+            .mput-success {
+                color: green;
+            }
+            
+            .mput-failure {
+                color: red;
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="col-md-12 well">
                 <h1 class="text-center"><?php echo $suiteName; ?></h1>
+                <hr>
+                <?php foreach ($structure as $name => $tests): ?>
+                <div>
+                    <h3><?php echo $name; ?></h3>
+                    <hr>
+                    <ul class="mput-tests">
+                    <?php foreach ($tests as $test): ?>
+                        <li class="mput-test <?php echo $test ['result'] ? 'mput-success' : 'mput-failure'; ?>">
+                            <?php echo $test ['message']; ?>
+                        </li>
+                    <?php endforeach; ?>
+                    </ul>
+                </div>    
+                <?php endforeach; ?>
             </div>
         </div>
     </body>    
